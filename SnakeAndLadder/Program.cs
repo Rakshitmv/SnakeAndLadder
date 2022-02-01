1ea5e26a8fdb822a958bc438f2559275261653cb
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 17:35:44 +0530

    [Rakshit] Add: UC1 single player game begin

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------Welcome to Snake and Ladder Game------");
+            SnakeAndLadder game = new SnakeAndLadder();
+            game.Play();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadder
+    {
+        public void Play()
+        {
+
+            int startPosition = 0;
+            Console.WriteLine("The game has begin");
+
+        }
+    }
+}
